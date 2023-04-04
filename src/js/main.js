e180ee1469e3c6968a55eee2338fae045622e8f8
Sasha Gordijchuk <san4ez1717@gmail.com>
2023-04-04 13:32:26 +0300

    currency toggle was implemented

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,45 @@ import $ from "jquery";
 import bootstrap from 'bootstrap';
 import '../css/main.scss';
 
+const MAX_USERS = 20;
+const MONTH_COUNT = 12;
+
 $(function(){
-  selectorFill(20);
-  handleSelect();
+  selectorFill('USD');
+  handleCurrencySelect();
+  handlePriceSelect();
 });
 
-const selectorFill = (num) => {
+function getPricePerMonth(currency) {
+  switch(currency) {
+    case 'USD':
+      return 35;
+    case 'EUR':
+      return 32;
+    default:
+      return 29;
+  }
+}
+
+function getCurrencySymbol(currency) {
+  switch(currency) {
+    case 'USD':
+      return '$';
+    case 'EUR':
+      return '€';
+    default:
+      return '£';
+  }
+}
+
+function selectorFill(currency) {
   let resultHTML = '';
 
-  for (let i = 1; i <= num; i++) {
-    resultHTML += '<li class="dropdown-item price-selector__dropdown-item" id="dropdown-item-' + i + '" role="button">'
+  for (let i = 1; i <= MAX_USERS; i++) {
+    resultHTML += '<li class="dropdown-item price-selector__dropdown-item" role="button">'
     + '<span class="price-selector__users">' + i + ' users</span>'
-    + '<span class="price-selector__price">$' + 420 * i + '/year</span>'
+    + '<span class="price-selector__price">'
+    + getCurrencySymbol(currency) + getPricePerMonth(currency) * MONTH_COUNT * i + '/year</span>'
     + '</li>';
   }
 
@@ -25,14 +52,30 @@ const selectorFill = (num) => {
   $('.price-selector__dropdown-menu').html(resultHTML);
 };
 
-const handleSelect = () => {
-  $('.price-selector__dropdown-item').on("click", function() {
+function handleCurrencySelect() {
+  $('.currencies__currency').on('click', function() {
+    const currency = $(this).text();
+    const usersCount = $('.price-selector__wrapper > .price-selector__users').text().split(' ')[0];
+    const currencySymbol = getCurrencySymbol(currency);
+    const pricePerMonth = getPricePerMonth(currency);
+    const priceText = currencySymbol + (pricePerMonth * usersCount * MONTH_COUNT);
+
+    $('.currencies__currency').removeClass('currencies__currency--selected');
+    $(this).addClass('currencies__currency--selected');
+    selectorFill(currency);
+    $('.price-selector__price').text(`${priceText}/year`);
+    $('.plan__price').text(currencySymbol + pricePerMonth)
+  })
+};
+
+function handlePriceSelect() {
+  $('.price-selector__dropdown-item').on('click', function() {
     const pumpHTML = '<span class="price-selector__pump">|</span>';
     let thisElementHTML = $(this).html();
     const newElementHTML = $(thisElementHTML)[0].outerHTML
     + pumpHTML
     + $(thisElementHTML)[1].outerHTML;
 
-    $(".price-selector__wrapper").html(newElementHTML);
+    $('.price-selector__wrapper').html(newElementHTML);
   })
 };
